ed9e58f99af128300c4f4476700940107a503133
T. Renee Armstrong <35774849+atraishelle@users.noreply.github.com>
2018-01-31 21:38:21 -0600

    Updated Rubric 2
    Assignment 2

---
+++ rubric2/students.js
@@ -0,0 +1,16 @@
+	// JavaScript Document
+var studentsName=prompt("Who is your student?");
+	
+switch(studentsName){
+	case "Renee":
+		alert ("Lucky");
+		break;
+		
+	case "Traishelle":
+		alert ("Even Better!");
+		break;
+		
+	default: 
+		alert ("That is not a students name!");
+}
+	
