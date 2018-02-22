64793d3ee7b54e1a5b551d907516253a1978dd86
T. Renee Armstrong <35774849+atraishelle@users.noreply.github.com>
2018-02-21 21:35:52 -0600

    changes
    changes

--- Rubric 2/students.js
+++ Rubric 2/students.js
@@ -1,16 +1,33 @@
-	// JavaScript Document
-var studentsName=prompt("Who is your student?");
-	
-switch(studentsName){
-	case "Renee":
-		alert ("Lucky");
-		break;
-		
-	case "Traishelle":
-		alert ("Even Better!");
-		break;
-		
-	default: 
-		alert ("That is not a students name!");
+
+
+var myNavs = document.getElementById("naviBars");
+
+console.log(myNavs.childNodes.length);
+
+var myNavs = document.getElementsByTagName("li");
+
+console.log ("Number of li links: "+ myNavs.length);
+
+var myNavs = myNavs.getElementsByTagName("li");
+
+console.log ("Number of Li items in Menu: "+ myNavs.length);
+	// intro button					
+function myIntro() {
+    document.getElementById("intro").innerHTML = "Dont forget to read!";
 }
-	
+//contact me button
+function myFunction() {
+    document.getElementsByTagName("P")[0].innerHTML = "contact@artbyreneea.com";
+} 
+
+function myPortfolio() {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode("Portfolio");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+}
+
+if (new Date().getHours() < 18) {
+    document.getElementById("welcome").innerHTML = "Good day!";
+}
+
