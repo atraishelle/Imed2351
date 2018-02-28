e65558cd9adc805e99ba84d7026d798f0faab2f0
T. Renee Armstrong <35774849+atraishelle@users.noreply.github.com>
2018-02-28 02:17:09 -0600

    Rubric 5
    new

---
+++ ReneeA Studios/js/renee.js
@@ -0,0 +1,68 @@
+// JavaScript Document
+//cookies
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+	
+	//
+	
+	
+var renee = document.getElementById("renee");
+var ill = document.getElementById("ill");
+var graph = document.getElementById("graph");
+var template = document.getElementById("template");
+
+renee.addEventListener("click", picLink);
+ill.addEventListener("click", picLink);
+graph.addEventListener("click", picLink);
+template.addEventListener("click", picLink);
+
+function picLink() {
+  var allImages = document.querySelectorAll("img");
+
+  for (var i = 0; i < allImages.length; i++) {
+    allImages[i].className = "hide";
+  }
+
+  var picId = this.attributes["data-img"].value;
+  var pic = document.getElementById(picId);
+  if (pic.className === "hide") {
+    pic.className = "";
+  } else {
+    pic.className = "hide";
+  }
+}
+
